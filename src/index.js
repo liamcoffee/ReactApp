caef15ab658aa98bfa1ed46cdbf68ad977ae47ae
Liam Baxter <liam@coffee-design.co.uk>
2019-04-14 18:03:05 +0100

    Finished Section 2

--- src/index.js
+++ src/index.js
@@ -4,8 +4,20 @@ import ReactDOM from "react-dom";
 
 // Create a react component
 
+const buttonText = "click me!";
+
 const App = () => {
-  return <div> Hello World!</div>;
+  return (
+    <div>
+      <label htmlFor="name" className="label">
+        Enter Name:
+      </label>
+      <input type="text" id="name" />
+      <button style={{ backgroundColor: "red", color: "#fff" }}>
+        {buttonText}
+      </button>
+    </div>
+  );
 };
 
 // Take the react component and display it
