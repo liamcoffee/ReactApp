541b8418c440b611b50b1615dc5d5980dffe844a
Liam Baxter <liam@coffee-design.co.uk>
2019-04-14 17:18:46 +0100

    Added Hello World

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Import the React and ReactDOM libraaries
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a react component
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  return <div> Hello World!</div>;
+};
+
+// Take the react component and display it
+
+ReactDOM.render(<App />, document.querySelector("#root"));
